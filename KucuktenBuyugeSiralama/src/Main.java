5e0c9eb52427046b2b3e1f8d2a943407c710ea3b
Oğuzhan Küreksiz <devoguzan@gmail.com>
2023-05-23 22:40:58 +0300

    Küçükten büyüğe doğru sıralaması için işaret değiştirildi.

--- KucuktenBuyugeSiralama/src/Main.java
+++ KucuktenBuyugeSiralama/src/Main.java
@@ -14,34 +14,34 @@ public class Main {
         System.out.print("3.sayiyi giriniz : ");
         sayi3 = input.nextInt();
 
-        if(sayi1>sayi2 && sayi1 > sayi3){
-            if (sayi2>sayi3){
-                System.out.println(sayi1 + " > " + sayi2 +" > " + sayi3);
+        if(sayi1<sayi2 && sayi1 < sayi3){
+            if (sayi2<sayi3){
+                System.out.println(sayi1 + " < " + sayi2 +" < " + sayi3);
             }
-            else if (sayi3>sayi2){
-                System.out.println(sayi1 + " > " + sayi3 +" > " + sayi2);
-            }
-            else{
-                System.out.println("Sayılar eşit de olabilir. O durumu karşılaştırmadık.");
-            }
-        }
-        else if(sayi2>sayi1 && sayi2>sayi3){
-            if (sayi1>sayi3){
-                System.out.println(sayi2 + " > " + sayi1 +" > " + sayi3);
-            }
-            else if(sayi3>sayi1) {
-                System.out.println(sayi2 + " > " + sayi3 +" > " + sayi1);
+            else if (sayi3<sayi2){
+                System.out.println(sayi1 + " < " + sayi3 +" < " + sayi2);
             }
             else{
                 System.out.println("Sayılar eşit de olabilir. O durumu karşılaştırmadık.");
             }
         }
-        else if (sayi3>sayi2 && sayi3>sayi1){
-            if (sayi1>sayi2){
-                System.out.println(sayi3 + " > " + sayi1 +" > " + sayi2);
+        else if(sayi2<sayi1 && sayi2<sayi3){
+            if (sayi1<sayi3){
+                System.out.println(sayi2 + " < " + sayi1 +" < " + sayi3);
             }
-            else if(sayi2>sayi1) {
-                System.out.println(sayi3 + " > " + sayi2 +" > " + sayi1);
+            else if(sayi3<sayi1) {
+                System.out.println(sayi2 + " < " + sayi3 +" < " + sayi1);
+            }
+            else{
+                System.out.println("Sayılar eşit de olabilir. O durumu karşılaştırmadık.");
+            }
+        }
+        else if (sayi3<sayi2 && sayi3<sayi1){
+            if (sayi1<sayi2){
+                System.out.println(sayi3 + " < " + sayi1 +" < " + sayi2);
+            }
+            else if(sayi2<sayi1) {
+                System.out.println(sayi3 + " < " + sayi2 +" < " + sayi1);
             }
             else{
                 System.out.println("Sayılar eşit de olabilir. O durumu karşılaştırmadık.");
